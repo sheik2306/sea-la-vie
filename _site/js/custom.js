9321bea9adf0eccfddc4d2dc1ef44ff01b7c5b44
sheik2306 <alex.dufour@outlook.com>
2025-02-07 17:30:00 -0500

    Deploy site

---
+++ _site/js/custom.js
@@ -0,0 +1,47 @@
+// Closes the sidebar menu
+$("#menu-close").click(function(e) {
+  e.preventDefault();
+  $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Map scrolling behaviour
+$(document).ready(function() {
+  $('#map_iframe').addClass('scrolloff');
+  $('#map').on('click', function () {
+    $('#map_iframe').removeClass('scrolloff');
+  });
+
+  $('#map_iframe').mouseleave(function  () {
+    $('#map_iframe').addClass('scrolloff');
+  });
+});
+
+$('#imageModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var slideTo = button.data('slide-to'); // Extract info from data-* attributes
+  var modal = $(this);
+  $('#carouselExampleIndicators').carousel(slideTo);
+});
